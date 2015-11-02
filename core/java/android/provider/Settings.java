a3a592417bf7b249dc7decf1a6f73762f88e83f8
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-11-02 00:02:36 -0500

    Use volume keys to control media volume anytime [1/2]
     * There's option volume_keys_control_ring_stream in Settings
       (Volume keys control ringtone volume / If on, volume keys
       control ringtone volume. If off, volume keys control media
       volume.) in Settings.
    
     * Modified to force media stream when it's on, notification/
       /ring stream when off
     * Use existing content resolver variable use one void for the
       setting getter
     * "ON" by default
    
    Signed-off-by: Funny-Bunny <dragos281993@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4040,6 +4040,13 @@ public final class Settings {
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
         /**
+         * Volume key controls ringtone or media sound stream
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_MEDIA_STREAM =
+                "volume_keys_control_media_stream";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
